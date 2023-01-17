32116042429cab2f275006ff9add5e1823a0066f
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-01-17 13:08:48 +0200

    EX14.1.js

---
+++ Ex14.1.js
@@ -0,0 +1,22 @@
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+// const MaxN = nums.reduce((max,n)=>{
+//     return Math.max(max,n);
+// });
+// console.log(MaxN);
+
+
+const sumEven = nums.reduce((a,b)=>{
+    if (b%2===0) {
+        return  a+b;
+    }
+    return a;
+},0);
+console.log(sumEven);
+
+const avg = nums.reduce((a,b)=>{
+    debugger
+    return (a+b);
+}) ;
+
+console.log(avg/nums.length);
