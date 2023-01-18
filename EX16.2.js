5319a1ce2be244346b34f7cc13acdc237d943338
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-01-18 13:22:56 +0200

    -EX16.2.js

---
+++ EX16.2.js
@@ -0,0 +1,61 @@
+var x = 10;
+console.log(x);//log 10
+if (true) {
+    var x = 20;
+    console.log(x);//log 20 function scoop
+}
+console.log(x);//log 20 overrwrite
+
+// --------------------------------------------- //
+
+var x = 10;
+console.log(x);//log 1
+if (true) {
+    (function () {
+        var x = 20;
+        console.log(x);//log 20
+    })();
+}
+console.log(x);//log 10 not change beacuse a function scoop
+
+// --------------------------------------------- //
+
+var x = 10;
+console.log(x);// log 10
+function test() {
+    var x = 20;
+    console.log(x);//log 20
+    if (x > 10) {
+        let x = 30;
+        console.log(x);//log 30
+    }
+    console.log(x);//log 20 let have small scoop than var
+}
+test();
+console.log(x);//log 10 function scoop
+// --------------------------------------------- //
+
+var x;
+x = 10;
+function test() {
+    var x;
+    if (x > 20) {
+        x = 50;
+    }
+    console.log(x);//undefinde 
+}
+test();
+
+// ----------------------------------- //
+
+function test() {
+    var x, y;
+    if (false) {
+        x = 50;
+    }
+    console.log(x);//undefinde
+    console.log(y);//undefinde
+    y = 100;
+    console.log(y);// log 100
+}
+test();
